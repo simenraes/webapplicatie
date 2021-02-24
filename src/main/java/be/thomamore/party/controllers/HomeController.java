e18061b56016c9d4152b2276707f774c1865363c
simenraes <simieraes@hotmail.com>
2021-02-24 23:02:52 +0100

    tot oef 2.3

--- src/main/java/be/thomamore/party/controllers/HomeController.java
+++ src/main/java/be/thomamore/party/controllers/HomeController.java
@@ -1,16 +1,21 @@
 package be.thomamore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final int mySpecialNumber = 729;
     @GetMapping({"/","/home"})
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "home";
     }
     @GetMapping("/about")
-    public String about(){
+    public String about(Model model){
+        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "about";
     }
+
 }
