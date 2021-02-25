2dcdc927d9b5c9c11fb83be02047ad06de2d16cd
simenraes <simieraes@hotmail.com>
2021-02-25 15:33:36 +0100

    localdate aangepast

--- src/main/java/be/thomamore/party/controllers/HomeController.java
+++ src/main/java/be/thomamore/party/controllers/HomeController.java
@@ -5,12 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
     private final int mySpecialNumber = 729;
     private final String[] venueNames = {"De Loods", "De Club", "De Hangar", "Zapoi", "Kuub", "Cuba Libre"};
+    private LocalDate datum = LocalDate.now();
 
 
     @GetMapping({"/", "/home"})
@@ -27,7 +29,7 @@ public class HomeController {
 
     @GetMapping("/pay")
     public String pay(Model model) {
-        model.addAttribute("LocalDate", LocalDateTime.now());
+        model.addAttribute("LocalDate", datum);
 
         return "pay";
     }
