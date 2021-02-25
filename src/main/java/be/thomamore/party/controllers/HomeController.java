51a4d1e9fa7d93b9b081bc762b6ae3bc3658ec0a
simenraes <simieraes@hotmail.com>
2021-02-25 15:00:29 +0100

    tot en met 3.5 bundel 1

--- src/main/java/be/thomamore/party/controllers/HomeController.java
+++ src/main/java/be/thomamore/party/controllers/HomeController.java
@@ -3,19 +3,47 @@ package be.thomamore.party.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
     private final int mySpecialNumber = 729;
-    @GetMapping({"/","/home"})
-    public String home(Model model){
+    private final String[] venueNames = {"De Loods", "De Club", "De Hangar", "Zapoi", "Kuub", "Cuba Libre"};
+
+
+    @GetMapping({"/", "/home"})
+    public String home(Model model) {
         model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "home";
     }
+
     @GetMapping("/about")
-    public String about(Model model){
+    public String about(Model model) {
         model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "about";
     }
 
+    @GetMapping("/pay")
+    public String pay(Model model) {
+        model.addAttribute("LocalDate", LocalDateTime.now());
+
+        return "pay";
+    }
+
+    @GetMapping({"/venuedetails", "/venuedetails/{venueName}"})
+    public String venuedetails(Model model,
+                               @PathVariable(required = false) String venueName) {
+        model.addAttribute("venueName", (venueName!=null) ? venueName : "--no venue chosen--");
+
+
+        return "venuedetails";
+    }
+
+    @GetMapping("/venuelist")
+    public String venueList(Model model) {
+        model.addAttribute("venueNames", venueNames);
+        return "venuelist";
+    }
 }
