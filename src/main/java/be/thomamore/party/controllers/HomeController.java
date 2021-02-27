b06d60b9e340b78dfd23657121b18d216aca7027
simenraes <simieraes@hotmail.com>
2021-02-27 16:47:51 +0100

    TOT EN MET 5.1 BUNDEL 1

--- src/main/java/be/thomamore/party/controllers/HomeController.java
+++ src/main/java/be/thomamore/party/controllers/HomeController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Calendar;
 
 @Controller
 public class HomeController {
     private final int mySpecialNumber = 729;
     private final String[] venueNames = {"De Loods", "De Club", "De Hangar", "Zapoi", "Kuub", "Cuba Libre"};
-    private LocalDate datum = LocalDate.now();
+    private final LocalDate datum = LocalDate.now(); // controller moet eig stateless zijn dusmoet ditniet final zijn ?
+    private final Calendar c1 = Calendar.getInstance();
 
 
     @GetMapping({"/", "/home"})
@@ -28,24 +30,36 @@ public class HomeController {
     }
 
     @GetMapping("/pay")
-    public String pay(Model model) {
+
+    public String pay(Model model,
+                      @PathVariable(required = false) boolean weekend) {
+
         model.addAttribute("LocalDate", datum);
+
 
         return "pay";
     }
 
-    @GetMapping({"/venuedetails", "/venuedetails/{venueName}"})
-    public String venuedetails(Model model,
-                               @PathVariable(required = false) String venueName) {
-        model.addAttribute("venueName", (venueName!=null) ? venueName : "--no venue chosen--");
-
-
+    @GetMapping({"/venuedetails", "/venuedetailsbyindex/{index}"})
+    public String venueDetails(Model model,
+                               @PathVariable(required = false)  Integer index) {
+        if (index!=null && index>=0 && index<venueNames.length ) {
+            model.addAttribute("venueName", venueNames[index]);
+            model.addAttribute("prevIndex", index>0 ? index-1 : venueNames.length-1);
+            model.addAttribute("nextIndex", index<venueNames.length-1 ? index+1 : 0);
+        }
         return "venuedetails";
+        //NOG EXCEPTION TOEVOEGEN ZIE BUNDEL P27
     }
+//model.addAttribute("venueName", (venueName!=null) ? venueName : "--no venue chosen--");
+
+
+
 
     @GetMapping("/venuelist")
     public String venueList(Model model) {
         model.addAttribute("venueNames", venueNames);
         return "venuelist";
     }
+
 }
